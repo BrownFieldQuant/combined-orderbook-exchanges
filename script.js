b95bb1dbbf0dd11f2eaf980abd726fee19d99211
David Khoa | Lowcost Ventures <bestwebmaker6789@gmail.com>
2026-07-25 17:24:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function setChartHeight() {
 	var windowHeight = window.innerHeight;
 	var chartContainer = document.getElementById('orderbookchart');
 	var available = windowHeight - chartContainer.offsetTop - 10;
-	var chartHeight = Math.max(260, Math.min(available, 520));
+	var chartHeight = Math.max(240, Math.min(available, 420));
 	chartContainer.style.height = chartHeight + 'px';
 }
 
